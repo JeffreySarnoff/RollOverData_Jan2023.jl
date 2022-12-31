22ce8eab64ce4a28ebc7f893ff42cbcd55858f94
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2022-12-31 11:11:12 -0500

    update

--- src/RollOverData.jl
+++ src/RollOverData.jl
@@ -1,6 +1,13 @@
 module RollOverData
 
+export Seq, seq,
+       roll, run,
+       window, slide, tile,
+       # from StatsBase
+       nobs, mean, var, std, skewness, kurtosis
 
+using StatsBase, VectorizedStatistics,
+      Accumulators, MovingRings
 
 end  # RollOverData
 
