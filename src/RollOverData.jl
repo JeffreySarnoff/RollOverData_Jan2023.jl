4a7efc4179fbb71e908919aa11dffcb62950b3b8
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2022-12-31 16:32:01 -0500

    from StatsBase

--- src/RollOverData.jl
+++ src/RollOverData.jl
@@ -4,7 +4,19 @@ export Seq, seq,
        roll, run,
        window, slide, tile,
        # from StatsBase
-       nobs, mean, var, std, skewness, kurtosis
+       nobs, minimum, maximum, extrema, span,
+       mean, var, std, skewness, kurtosis,
+       genmean, genvar, geomean, harmmean,
+       median, middle, mode,
+       mad, variation,
+       autocor, autocov,
+       entropy, 
+       # from StatsBase f(x,y)
+       maxad, meanad, msd, rmsd,
+       cor, crosscor, corkendall, corspearman,
+       cov, crosscov,
+       crossentropy,
+       psnrm
 
 using StatsBase, VectorizedStatistics,
       Accumulators, MovingRings
