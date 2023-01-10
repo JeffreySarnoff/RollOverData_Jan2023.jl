b0748e2ef45636602f04fe4483d44f61097b507b
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2023-01-10 06:42:15 -0500

    Update roll.jl

--- src/roll.jl
+++ src/roll.jl
@@ -1,7 +1,19 @@
+struct Pad{T}
+    padding::T
+    padfirst::Bool
+    padlast::Bool
+    padfirstn::Int
+    padlastn::Int
+end
+
+Pad(padding::T; padfirst::Bool=false,padlast::Bool=false,padfirstn::Int=0,padlastn::Int=0) where {T} =
+    Pad{T}(padding, padfirst, padlast, padfirstn, padlastn)
+
+NoPad = Pad(nothing)
+
 roll(data, fn::Function) = map(fn, data)
 
-function roll(data, windowspan::Integer, fn::Function;  
-              padfirst::Union{Bool,Int}=false, padlast::Union{Bool,Int}=false, padding=missing)
+function roll(data, windowspan::Integer, fn::Function; padding::Pad=NoPad)
     datarows = nrows(data)
     trimrows = rolledrows(datarows, windowspan)
     trimrows >= windowspan || TrimSpanError(trimrows, windowspan)
@@ -9,11 +21,25 @@ function roll(data, windowspan::Integer, fn::Function;
     rows_trimmed = trimmedrows(windowspan)
     rettype = rts(fn, (eltype(data),))
     
-    padfirst = padfirst == 0 ? false : padfirst
-    padlast = padlast == 0 ? false : padlast
+    padfirst = padding.padfirst || padding.padfirstn > 0
+    padlast = padding.padlast || padding.padlastn > 0
   
-    if padfirst && padlast
-       padbgn = div(trimrows+1, 2)
+    if padding.padfirst && padding.padfirstn == 0
+       if padding.padlastn == 0
+           padding.padfirstn = rows_trimmed
+       else
+           padding.padfirstn = rows_trimmed - padding.padlastn
+       end
+    elseif padding.padlast && padding.padlastn == 0
+       if padding.padfirstn == 0
+           padding.padlastn = rows_trimmed
+       else
+           padding.padlastn = rows_trimmed - padding.padfirstn
+       end
+    end
+  
+    if padding.padfirst && padding.padlast
+       padb = div(trimrows+1, 2)
        padend = rows_trimmed - padbgn
        result  = Vector{Union{rettype, typeof(padding)}}(undef, datarows)
        result[1:padbgn] .= padding
