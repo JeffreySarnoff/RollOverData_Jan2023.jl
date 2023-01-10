9b92e7e16a992fcfdb8858e867a6b4f53b11a5a8
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2023-01-10 06:06:27 -0500

    Update roll.jl

--- src/roll.jl
+++ src/roll.jl
@@ -1,10 +1,39 @@
 roll(data, fn::Function) = map(fn, data)
 
-function roll(data, windowspan::Integer, fn::Function; 
-              padfirst=false, padlast=false, padding=missing)
+function roll(data, windowspan::Integer, fn::Function;  
+              padfirst::Union{Bool,Int}=false, padlast::Union{Bool,Int}=false, padding=missing)
     datarows = nrows(data)
     trimrows = rolledrows(datarows, windowspan)
     trimrows >= windowspan || TrimSpanError(trimrows, windowspan)
+
+    rows_trimmed = trimmedrows(windowspan)
+    rettype = rts(fn, (eltype(data),))
+    
+    padfirst = padfirst == 0 ? false : padfirst
+    padlast = padlast == 0 ? false : padlast
+  
+    if padfirst && padlast
+       padbgn = div(trimrows+1, 2)
+       padend = rows_trimmed - padbgn
+       result  = Vector{Union{rettype, typeof(padding)}}(undef, datarows)
+       result[1:padbgn] .= padding
+       result[datarows-padend:datarows] .= padding
+       result[padbgn+1:datarows-padend-1] .= zero(rettype)
+    end
+    
+    if !padfirst && !padlast
+       result  = Vector{rettype}(undef, rows_trimmed)
+    else
+       result  = Vector{rettype}(undef, datarows)
+    end
+  
+    if padfirst || padlast
+       result = zeros(eltype(data), datarows)
+    else
+       result = zeros(eltype(data), trimrows)
+    end
+    if padfirst
+       
     slider = roll_sliding(datarows, windowspan)
 end
 
