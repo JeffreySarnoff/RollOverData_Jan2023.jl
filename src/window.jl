83b74927a2141f146513910e8892698aa9113cb5
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2023-01-10 03:56:13 -0500

    Update window.jl

--- src/window.jl
+++ src/window.jl
@@ -1,6 +1,18 @@
 rolledrows(datarows, windowspan) = datarows - trimmedrows(windowspan)
 trimmedrows(windowspan) = windowspan - 1
 
+roll_sliding(datarows, windowspan) = 1:1:rolledrows(datarows, windowspan)
+roll_tiling(datarows, windowspan) = 1:windowspan:rolledrows(datarows, windowspan)
+roll_tiling(datarows, windowspan, tilespan) = 1:tilespan:rolledrows(datarows, windowspan)
+
+run_sliding(datarows, windowspan) = 1:1:datarows
+run_tiling(datarows, windowspan) = 1:windowspan:datarows
+run_tiling(datarows, windowspan, tilespan) = 1:tilespan:datarows
+
+
+
+
+
 
 
 
