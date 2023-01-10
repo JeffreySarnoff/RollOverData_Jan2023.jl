2e9f022e22e174bfb6a36cd464b4884b08d706b0
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2023-01-10 04:48:44 -0500

    Update roll.jl

--- src/roll.jl
+++ src/roll.jl
@@ -7,8 +7,8 @@ end
 
 function roll(data, windowspan::Integer, tilespan::Integer, fn::Function; padfirst=false, padlast=false, padding=missing)
     datarows = nrows(data)
-    datarows >= windowspan || throw(SpanError(datarows, windowspan))
     trimrows = trimmedrows(windowspan)
+    trimrows >= windowspan || throw(SpanError(datarows, windowspan))
     trimrows >= tilespan || throw(TileError(trimrows, tilespan))
     window_reps = divrem(trimrows, windowspan)
     tile_reps = divrem(trimrows, tilespan)
