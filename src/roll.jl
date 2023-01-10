e024d4fdd08f3fab3de0a8e901d5834baf894298
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2023-01-10 04:30:09 -0500

    update

--- src/roll.jl
+++ src/roll.jl
@@ -1,11 +1,11 @@
 
-function roll(data, windowspan::I, fn::F; padfirst=false, padlast=false, padding=missing) where {I<:Integer, F<:Function}
+function roll(data, windowspan::Integer, fn::Function; padfirst=false, padlast=false, padding=missing)
     datarows = nrows(data)
     datarows >= windowspan || throw(SpanError(datarows, windowspan))
     slider = roll_sliding(datarows, windowspan)
 end
 
-function roll(data, windowspan::I, tilespan::I, fn::F; padfirst=false, padlast=false, padding=missing) where {I<:Integer, F<:Function}
+function roll(data, windowspan::Integer, tilespan::Integer, fn::Function; padfirst=false, padlast=false, padding=missing) where {I<:Integer, F<:Function}
     datarows = nrows(data)
     datarows >= windowspan || throw(SpanError(datarows, windowspan))
     tiler  = roll_tiling(datarows, windowspan, tilespan)
