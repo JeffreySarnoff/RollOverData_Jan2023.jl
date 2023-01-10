3038b5d88df86ec33e1eaa498f4b9c80cd9da94f
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2023-01-10 05:53:05 -0500

    Update utils.jl

--- src/utils.jl
+++ src/utils.jl
@@ -17,3 +17,12 @@ nrows(x) = isempty(size(x)) ? 1 : size(x)[1]
 ncols(x::AbstractVector) = 1
 ncols(x::AbstractArray) = size(x)[2]
 ncols(x) = isempty(size(x)) ? 1 : size(x)[2]
+
+# returned types (rough)
+
+@inline rts(fn) = Base.return_types(fn)[max(1, end-1)]
+@inline rts(fn, typs) = Base.return_types(fn, typs)[max(1, end-1)]
+
+@inline nrts(fn) = max(1, length(rts(fn).parameters))
+@inline nrts(fn, typs) = max(1, length(rts(fn,typs).parameters))
+
