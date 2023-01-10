5a83f904441974be0eff03fb41c736b8372a3576
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2023-01-10 04:51:55 -0500

    Update roll.jl

--- src/roll.jl
+++ src/roll.jl
@@ -1,17 +1,21 @@
 
 function roll(data, windowspan::Integer, fn::Function; padfirst=false, padlast=false, padding=missing)
     datarows = nrows(data)
-    datarows >= windowspan || throw(SpanError(datarows, windowspan))
+    trimrows = trimmedrows(windowspan)
+    trimrows >= windowspan || throw(TrimSpanError(trimrows, windowspan))
     slider = roll_sliding(datarows, windowspan)
 end
 
 function roll(data, windowspan::Integer, tilespan::Integer, fn::Function; padfirst=false, padlast=false, padding=missing)
     datarows = nrows(data)
     trimrows = trimmedrows(windowspan)
-    trimrows >= windowspan || throw(SpanError(datarows, windowspan))
-    trimrows >= tilespan || throw(TileError(trimrows, tilespan))
+    trimrows >= windowspan || throw(TrimSpanError(trimrows, windowspan))
+    trimrows >= tilespan || throw(TrimTileError(trimrows, tilespan))
+
     window_reps = divrem(trimrows, windowspan)
     tile_reps = divrem(trimrows, tilespan)
     
     tiler  = roll_tiling(datarows, windowspan, tilespan)
+    
+    window_reps, tile_reps
 end
