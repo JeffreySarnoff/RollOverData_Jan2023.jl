7da71c4fcd36ba3da3a78d485e3c34bdc42cf48b
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2023-01-10 04:33:22 -0500

    correct

--- src/roll.jl
+++ src/roll.jl
@@ -5,7 +5,7 @@ function roll(data, windowspan::Integer, fn::Function; padfirst=false, padlast=f
     slider = roll_sliding(datarows, windowspan)
 end
 
-function roll(data, windowspan::Integer, tilespan::Integer, fn::Function; padfirst=false, padlast=false, padding=missing) where {I<:Integer, F<:Function}
+function roll(data, windowspan::Integer, tilespan::Integer, fn::Function; padfirst=false, padlast=false, padding=missing)
     datarows = nrows(data)
     datarows >= windowspan || throw(SpanError(datarows, windowspan))
     tiler  = roll_tiling(datarows, windowspan, tilespan)
