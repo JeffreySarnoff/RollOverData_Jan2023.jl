c466a6f4647ac6abe778ffb965e500c95a23d2bd
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2023-01-10 04:46:28 -0500

    Update exceptions.jl

--- src/exceptions.jl
+++ src/exceptions.jl
@@ -2,12 +2,19 @@ struct SpanError <: Exception
     msg::String
 end
 
+struct TileError <: Exception
+    msg::String
+end
+
 struct WeightError <: Exception
     msg::String
 end
 
 SpanError(seqlength, windowspan) =
     SpanError("\n\tBad window span ($windowspan) for length $seqlength.\n" )
+
+TileError(seqlength, tilespan) =
+    SpanError("\n\tBad tile span ($tilespan) for length $seqlength.\n" )
 
 WeightError(nweighting, windowspan) =
     WeightError("\n\twindowspan ($windowspan) != length(weighting) ($nweighting))).\n" )
