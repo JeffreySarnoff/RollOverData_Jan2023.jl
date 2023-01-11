b0ca74ebc321d8bf51fe6ec0e25b42ab32e3d713
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2023-01-11 05:44:12 -0500

    Update basic_roll.jl

--- src/basic_roll.jl
+++ src/basic_roll.jl
@@ -21,8 +21,36 @@
        result = map(i->fn(data[1+i:windowspan+i]), addtospan)
 =#
 
-function basic_roll(data, windowspan::Integer, fn::Function)
+function basic_roll(data::D, windowspan::Integer, fn::Function) where {N,T,D<:Union{Vector{T},NTuple{N,T}}}
     nwindows = 0:nrows(data)-windowspan
     map(i->fn(data[1+i:windowspan+i]), nwindows)
 end
+
+function basic_roll(data::D, windowspan::Integer, fn::Function, result) where {N,T,D<:Union{Vector{T},NTuple{N,T}}}
+    wspan = windowspan-1
+    nwindows = 1:nrows(data)-windowspan+1
+    for i in nwindows
+        result[i] = fn(data[i:wspan+i])
+    end
+    result
+end
+
+function basic_roll(data::T, windowspan::Integer, fn::Function) where {T<:AbstractArray}
+    nwindows = 0:nrows(data)-windowspan
+    ncolumns = ncols(data)
+    map(i->fn(data[1+i:windowspan+i,1:ncolumns]), nwindows)
+end
+
+function basic_roll(data::T, windowspan::Integer, fn::Function) where {T<:AbstractArray}
+    hcat(map(c->basic_roll(data2[:,c], windowspan, fn), 1:ncols(data2))...)
+end
+
+# result = (similar(ans) .= 0.0)
+function basic_roll!(data::T, windowspan::Integer, fn::Function, result) where {T<:AbstractArray}
+    for c in 1:ncols(data)
+        result[:,c] = basic_roll(data2[:,c], windowspan, fn)
+    end
+    result
+end
+
 
