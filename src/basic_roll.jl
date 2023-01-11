6526d3b6e60f2097313631e7f1d03344f76e2fbb
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2023-01-11 04:32:33 -0500

    Update basic_roll.jl

--- src/basic_roll.jl
+++ src/basic_roll.jl
@@ -6,8 +6,18 @@
       (windowsize - 1) evaluands from the result.
    The initial (windowsize - 1) data elements are used to
       resolve the first value of the windowed function.
+
+   for example
+       data = [1,2,3,4,5,6]; windowspan = 4; fn = mean;
+       datalen = length(data) # == 6;
+       max_addtospan = datalen - windowspan # == 2
+       addtospan = 0:max_addtospan
+
+       the result contains three means
+           mean(data[1+0:windowspan+0])
+           mean(data[1+1:windowspan+1])
+           mean(data[1+2:windowspan+2])
+
+       result = map(i->fn(data[1+i:windowspan+i]), addtospan)
 =#
 
-
-     
-=#
