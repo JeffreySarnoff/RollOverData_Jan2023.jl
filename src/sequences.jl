dc5db42aeb9275ac3fb5e19e2143cc785590379c
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2023-01-12 03:21:19 -0500

    Update sequences.jl

--- src/sequences.jl
+++ src/sequences.jl
@@ -4,7 +4,7 @@
 """
     Seq
 """
-const Seq = Union{AbstractArray{T,N},NTuple{N,T}} where {N,T}
+const Seq = Union{AbstractArray{T,N}, NTuple{N,T}} where {N,T}
 
 """
     seq
@@ -12,3 +12,13 @@ const Seq = Union{AbstractArray{T,N},NTuple{N,T}} where {N,T}
 seq(x::AbstractArray{T,N}) where {N,T} = x
 seq(x::NTuple{N,T}) where {N,T} = x
 
+function rangeup(start, step, stop)
+    span = stop - start
+    multisteps, singlesteps = divrem( span, step )
+    singlestepsup = fma(-multisteps, step, stop)
+    startfrom = span - singlestepsup
+    startfrom
+end
+
+    
+subseq(x::Seq, subpart::LinRange
